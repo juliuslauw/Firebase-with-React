7d277a3728f77ee8e22f1bc743480dcaa4a6dc58
juliuslauw <julauw@g.hmc.edu>
2018-10-09 13:37:33 -0700

    Updated user interface after user's login

--- fun-food-friends/src/App.js
+++ fun-food-friends/src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
     const itemsRef = firebase.database().ref('items');
     const item = {
       title: this.state.currentItem,
-      user: this.state.username
+      user: this.state.user.displayName || this.state.user.email
     }
     itemsRef.push(item);
     this.setState({
@@ -58,6 +58,11 @@ class App extends Component {
   }
 
   componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      }
+    });
     const itemsRef = firebase.database().ref('items');
     itemsRef.on('value', (snapshot) => {
       let items = snapshot.val();
@@ -82,44 +87,47 @@ class App extends Component {
     return (
       <div className='app'>
         <header>
-            <div className="wrapper">
-              <h1>Fun Food Friends</h1>
-
-            </div>
-        </header>
-        <div className="wrapper">
-          <h1>Fun Food Friends</h1>
-          {this.state.user ?
-            <button onClick={this.logout}>Log Out</button>
+          <div className="wrapper">
+            <h1>Fun Food Friends</h1>
+            {this.state.user ?
+              <button onClick={this.logout}>Logout</button>
             :
-            <button onClick={this.login}>Log In</button>
-          }
-        </div>
-        <div className='container'>
-          <section className='add-item'>
-                <form onSubmit={this.handleSubmit}>
-                  <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
-                  <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
-                  <button>Add Item</button>
-                </form>
-          </section>
-          <section className='display-item'>
+              <button onClick={this.login}>Log In</button>
+            }
+          </div>
+        </header>
+        <div className='app'>
+            <header>
               <div className="wrapper">
-                <ul>
-                  {this.state.items.map((item) => {
-                    return (
-                      <li key={item.id}>
-                        <h3>{item.title}</h3>
-                        <p>brought by: {item.user}
-                          <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
-                        </p>
-                      </li>
-                    )
-                  })}
-                </ul>
+                <h1>Fun Food Friends</h1>
+                {this.state.user ?
+                  <button onClick={this.logout}>Logout</button>
+                  :
+                  <button onClick={this.login}>Log In</button>
+                }
               </div>
-          </section>
-        </div>
+            </header>
+            {this.state.user ?
+              <div>
+                <div className='user-profile'>
+                  <img src={this.state.user.photoURL} />
+                </div>
+                <div className='container'>
+                  <section className='add-item'>
+                    <form onSubmit={this.handleSubmit}>
+                      <input type="text" name="username" placeholder="What's your name?" value={this.state.user.displayName || this.state.user.email} />
+                      <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
+                      <button>Add Item</button>
+                    </form>
+                  </section>
+                </div>
+              </div>
+              :
+              <div className='wrapper'>
+                <p>You must be logged in to see the potluck list and submit to it.</p>
+              </div>
+            }
+          </div>
       </div>
     );
   }
